8b597c32908aa0988f9fea080d7774d98027e709
jtdutta1 <jtdutta1@gmail.com>
2025-11-15 22:14:18 +0530

    feat: Added motion atributes to Particle

--- src/core/particle/particle.cc
+++ src/core/particle/particle.cc
@@ -28,7 +28,42 @@ void Particle::setY(int y) {
     this->POS_Y = y;
 }
 
+float Particle::getRadius() {
+    return this->RADIUS;
+}
+
+void Particle::setRadius(float radius) {
+    this->RADIUS = radius;
+}
+
+std::shared_ptr<Momentum> Particle::getMomentum() {
+    return this->MOMENTUM;
+}
+
+void Particle::setMomentum(std::shared_ptr<Momentum> momentum) {
+    this->MOMENTUM->mass = momentum->mass;
+    this->MOMENTUM->velocity = momentum->velocity;
+}
+
+float Particle::getMass() {
+    return this->MOMENTUM->mass;
+}
+
+void Particle::setMass(float mass) {
+    this->MOMENTUM->mass = mass;
+}
+
+float Particle::getVelocity() {
+    return this->MOMENTUM->velocity;
+}
+
+void Particle::setVelocity(float velocity) {
+    this->MOMENTUM->velocity = velocity;
+}
+
 void Particle::display(){
-    std::vector<int> pos = this->getPos();
-    std::cout << "Particle(" << pos[0] << ", " << pos[1] << ")\n";
+    std::cout << "Particle(POS=[" << this->getX() << ", " << this->getY() 
+              << "], RADIUS=" << this->getRadius() 
+              << ", MASS=" << this->getMass()
+              << ", VELOCITY=" << this->getVelocity() << ")\n";
 }
--- src/core/particle/particle.h
+++ src/core/particle/particle.h
@@ -1,16 +1,25 @@
 #include <vector>
+#include <memory>
+#include "motion.h"
 
 class Particle{
     /*
-    Class encapsulates a basic particle. It currently holds the position of a particle in a world
+    Class encapsulates a basic particle (a circle). It has the following properties
+    - Position (Current position in a 2D plane)
+    - Momentum (Record of motion possesed by the body. Will store both mass and velocity)
+    - Radius (Size of the particle)
     */
     private:
         int POS_X, POS_Y;
-    
+        std::shared_ptr<Momentum> MOMENTUM;
+        float RADIUS;
     public:
         // Constructors
-        Particle(): POS_X(0), POS_Y(0) {}
-        Particle(int x, int y): POS_X(x), POS_Y(y) {}
+        Particle(): POS_X(0), POS_Y(0), MOMENTUM(std::make_shared<Momentum>()), RADIUS(1.0f) {}
+        Particle(int x, int y): POS_X(x), POS_Y(y), MOMENTUM(std::make_shared<Momentum>()), RADIUS(1.0f) {}
+        Particle(int x, int y, float r): POS_X(x), POS_Y(y), MOMENTUM(std::make_shared<Momentum>()), RADIUS(r) {}
+        Particle(int x, int y, std::shared_ptr<Momentum> momentum): POS_X(x), POS_Y(y), MOMENTUM(std::make_shared<Momentum>(momentum->mass, momentum->velocity)), RADIUS(1.0f) {}
+        Particle(int x, int y, std::shared_ptr<Momentum> momentum, float r): POS_X(x), POS_Y(y), MOMENTUM(std::make_shared<Momentum>(momentum->mass, momentum->velocity)), RADIUS(r) {}
 
         // Particle customizer methods
         void setPos(int x, int y);
@@ -21,6 +30,18 @@ class Particle{
 
         int getY();
         void setY(int y);
+
+        float getRadius();
+        void setRadius(float radius);
+
+        std::shared_ptr<Momentum> getMomentum();
+        void setMomentum(std::shared_ptr<Momentum> momentum);
+
+        float getMass();
+        void setMass(float mass);
+
+        float getVelocity();
+        void setVelocity(float velocity);
 
 
         // Show Particle
